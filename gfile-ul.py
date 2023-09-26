2c1bed8798c2e84d08d82ee354eda558c91fc985
floodslake <129939125+floodslake@users.noreply.github.com>
2023-09-27 00:36:08 +0700

    Update gfile-ul.py

--- gfile-ul.py
+++ gfile-ul.py
@@ -3,10 +3,11 @@ from gfile import GFile
 
 def upload_folder(folder,output):
     for x in sorted(os.listdir(folder)):
-        url = GFile(x, progress=True).upload().get_download_page()
+        url = GFile(x, progress=False).upload().get_download_page()
         print(url)
 
 def main():
+    parser = argparse.ArgumentParser()
     parser.add_argument('folder', help='Folder name')
     args = parser.parse_args()
 
