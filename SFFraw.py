2639b24b81943aad0bd62943fc8b78b00388e94d
floodslake <129939125+floodslake@users.noreply.github.com>
2024-01-17 18:51:30 +0700

    Update SFFraw.py

--- SFFraw.py
+++ SFFraw.py
@@ -13,6 +13,6 @@ s_referer = '--http-header "Referer=%s"' % (referer)
 if use_ffmpeg == 'true':
   download_command = 'ffmpeg -hide_banner -i "%s" -c copy -map_metadata -1 -map_metadata:s -1 "DL/%s.ts"' % (m3u8_url, output)
 else:
-  download_command = 'streamlink --stream-timeout 300 --stream-segment-attempts 300 --hls-playlist-reload-attempts 300 --hls-live-restart --retry-open 300 --retry-streams 30 --retry-max 600 --stream-segment-threads 8 --twitch-disable-hosting --twitch-disable-ads --twitch-disable-reruns --force-progress %s %s -o "DL/%s.ts" "%s" best' % (http_header, s_referer, output, m3u8_url)
+  download_command = 'streamlink --stream-timeout 300 --stream-segment-attempts 300 --hls-playlist-reload-attempts 300 --hls-live-restart --retry-open 300 --retry-streams 30 --retry-max 600 --stream-segment-threads 8 --twitch-disable-hosting --twitch-disable-ads --twitch-disable-reruns --progress=force %s %s -o "DL/%s.ts" "%s" best' % (http_header, s_referer, output, m3u8_url)
   
 os.system(download_command)
