f59e748723f5ba9ceae67560cb3cb31a125b1647
floodslake <129939125+floodslake@users.noreply.github.com>
2024-06-19 21:35:35 +0700

    Update getServer.py

--- getServer.py
+++ getServer.py
@@ -4,15 +4,18 @@ import time
 while True:
     try:
         getServer = requests.get("https://api.gofile.io/getServer").json()
+        
         if getServer['status']=="ok":
             try:
                 server = getServer['data']['servers'][0]['name']
-                except KeyError:
-                    server = getServer['data']['server']
-                if server!="store10":
-                    break
-                else:
-                    print(server)
-                    time.sleep(2)
+            except KeyError:
+                server = getServer['data']['server']
+                
+            if server!="store10":
+                break
+            else:
+                print(server)
+                time.sleep(2)
+
     except requests.exceptions.RequestException as e:
         time.sleep(30)
