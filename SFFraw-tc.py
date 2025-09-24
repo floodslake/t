e83ad4076702f9464b8bf3d88ebbf8acfd5d1265
floodslake <129939125+floodslake@users.noreply.github.com>
2025-09-24 19:24:13 +0700

    Raw tc (#13)
    * Create 02raw-tc.yml
    
    * Update 02raw-tc.yml
    
    * Create SFFraw-tc.py
    
    * Update 02raw-tc.yml
    
    * Update SFFraw-tc.py
    
    * Update SFFraw-tc.py
    
    * Update SFFraw-tc.py

--- SFFraw-tc.py
+++ SFFraw-tc.py
@@ -11,6 +11,8 @@ output = "%s-[%s]" % (file_name, unix2base62.timename())
 http_header = '--http-header "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0"'
 http_cookie = '--http-cookie "%s"' % (cookies)
 s_referer = '--http-header "Referer=%s"' % (referer)
+new_m3u8_url = m3u8_url.replace("/132.68/media", "/138.96/media")
 
-download_command = 'streamlink --stream-timeout 300 --stream-segment-attempts 300 --hls-playlist-reload-attempts 300 --hls-live-restart --retry-open 300 --retry-streams 30 --retry-max 600 --stream-segment-threads 8 --twitch-disable-hosting --twitch-disable-ads --twitch-disable-reruns --progress=force %s %s %s -o "DL/%s.ts" "%s" best' % (http_header, http_cookie, s_referer, output, m3u8_url)
+download_command = 'streamlink --stream-timeout 300 --stream-segment-attempts 300 --hls-playlist-reload-attempts 300 --hls-live-restart --retry-open 300 --retry-streams 30 --retry-max 600 --stream-segment-threads 8 --twitch-disable-hosting --twitch-disable-ads --twitch-disable-reruns --progress=force %s %s %s -o "DL/%s.ts" "%s" best' % (http_header, http_cookie, s_referer, output, new_m3u8_url)
+
 os.system(download_command)
